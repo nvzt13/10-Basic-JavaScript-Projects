f6b00f64d0935a949cf802094533f7b1c0520612
Nevzat Atalay <nevzatatalay79@gmail.com>
2023-11-25 13:03:51 +0300

    uploaded ninth project

--- 9-)Cinema-Seat-Project/app.js
+++ 9-)Cinema-Seat-Project/app.js
@@ -2,21 +2,36 @@ const container = document.querySelector(".container")
 const count = document.getElementById("count")
 const amount = document.getElementById("amount")
 const select = document.getElementById("movie")
+const seats = document.querySelectorAll(".seat:not(.reserved)")
 
+// koltuklara tıklanınca oluşan addevent
 container.addEventListener("click", function(e){
+    //koltuk seat sınıfını içeriyorsa ve reserved sınıfını içermiyorsa işlem yap
     if(e.target.classList.contains("seat") && !e.target.classList.contains("reserved") ){
         e.target.classList.toggle("selected")
+        
         calculaterTotal()
     }
 })
-
+// film değişirse
 select.addEventListener("change",function(e){
     calculaterTotal()
 })
 
+//kaç koltuk seçildi ve fiyatını hesaplayan  fonksiyon
 function calculaterTotal(){
-    let countSelectedSeat = container.querySelectorAll(".seat.selected").length;
-    let price = select.value * countSelectedSeat
-    amount.innerHTML = price
+    // 
+    const selectedSeat = container.querySelectorAll(".seat.selected")
+    //seçili koltukları içeren dizi
+    const selectedSeatArr = [];
+    //bütün koltukların içeren dizi(reserve hariç)
+    const seatsArr = [];
+
+    selectedSeat.forEach(function(seat){
+        selectedSeat.push(seat)
+    })
+
+    let countSelectedSeat = selectedSeat.length;
+    amount.innerHTML = select.value * countSelectedSeat
     count.innerHTML = countSelectedSeat
 }
