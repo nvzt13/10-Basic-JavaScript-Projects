01ef7e3cf5ee6bcd883d9945e74c86f7f413420c
Nevzat Atalay <nevzatatalay79@gmail.com>
2023-11-28 22:00:40 +0300

    opdate app.js

--- 10-)Hanging-Man/app.js
+++ 10-)Hanging-Man/app.js
@@ -1,25 +1,62 @@
 const word = document.getElementById("word")
-const correctLetters = ["j", "a"," "]
-const wrongLetters = []
+const popup = document.getElementById("popup-container")
+const message = document.getElementById("succes-message")
+const correctLetters = ["j", "a","v",]
+const wrongLetters = [];
+const selectedWord =  getRandomWord()
+
 //Random word function
 
+displayWord()
+
 function getRandomWord(){
-    const words = ["javascript","phyton","node","react","css","html"]
+    const words = ["javascript","phyton","node","react","css","html","java"]
     return words[Math.floor(Math.random() * words.length)]
 }
 
 // Display word innnerHTML function
 
 function displayWord(){
-    const selectedWord =  getRandomWord()
 
+    //Get random word
+
+    //Display and split word html
     word.innerHTML = `
     ${selectedWord.split('').map(letter => `
         <div class="letter"> 
-            ${correctLetters.includes(letter) ? letter : " "}
+            ${correctLetters.includes(letter) ? letter: ""}
         </div>
     `).join('')}
    `;
-   console.log(word.innerText)
+
+    //search letter and compare
+   const w = word.innerText.replace(/\n/g,'')
+   if(w === selectedWord){
+    popup.style.display = "flex"
+    message.innerText = " Tebrikler Kazandınız"
 }
-displayWord()
+  
+}
+
+// Add event listener keydown
+
+window.addEventListener("keydown",function(e){
+    if(e.keyCode >= 65 && e.keyCode <= 90){
+         const letter = e.key
+         if(selectedWord.includes(letter)){
+          if(!correctLetters.includes(letter)){
+            correctLetters.push(letter)
+            displayWord()
+          }
+         }else{
+           console.log("bu harfi zaten eklediniz")
+         }
+    }else{
+        if(!wrongLetters.includes(letter)){
+            wrongLetters.push(letter)
+            //Wrong letter updadte
+            console.log("hata")
+        }
+    }
+ 
+})
